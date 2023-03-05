39f09cd936e4f961642effcdfbbc6b2a342e43eb
ocetot <perisjara@gmail.com>
2023-03-05 02:59:22 -0300

    subiendo el 2 video funcionando

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
-// Exportamos un objeto que contiene la configuración de nuestra aplicación
+require('dotenv').config();
+
 module.exports = {
-    // Dentro del objeto, definimos una propiedad llamada "app"
+    
     app:{
-        // La propiedad "port" indica el número de puerto en el que se ejecutará nuestra aplicación
-        // Utilizamos process.env.PORT para obtener el valor de una variable de entorno llamada "PORT", 
-        // si no está definida, utilizamos el valor 4000 por defecto.
+        
         port: process.env.PORT || 4000,
     }
 }
--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+const respuestas = require('../../red/Respuestas')
+
 const routes = express.Router();
 
 routes.get('/', function(req, res){
-    res.send('Cliente OK')
+    respuestas.success(req, res, 'Todos ok desde clientes', 200)
 })
 
 module.exports = routes;
